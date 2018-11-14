91eb2c02b64e0586f70af5b0ad6e46eef369bd2b
aish12 <ashashidhar@utexas.edu>
2018-11-13 20:13:29 -0600

    updated vars

--- smbattack.py
+++ smbattack.py
@@ -1,7 +1,7 @@
 import argparse as ag
 import os 
 
-def performAttack(location, servername, username, password=None):
+def performAttack(location, serverIP, username, password=None):
 	if (password is None):
 		print ("Null Password, brute forced to: " + bruteForcePassword())
 	else:
@@ -12,14 +12,14 @@ def bruteForcePassword():
 	return "bruteForcePassword"
 parser = ag.ArgumentParser()
 parser.add_argument('--mountloc', type=str, help="Enter the location to mount the system")
-parser.add_argument('--servername', type=str, help="Enter the name of the targer windows server")
+parser.add_argument('--serverIP', type=str, help="Enter the name of the targer windows server")
 parser.add_argument('--username', type=str, help="Enter the username of the target windows server")
 parser.add_argument('--password', type=str, help="Optional. Enter the password of the target windows server. If not included, password will be brute forced.")
 
 
 args = parser.parse_args()
 if args.password:
-	performAttack(args.mountloc, args.servername, args.username, args.password)
+	performAttack(args.mountloc, args.serverIP, args.username, args.password)
 else:
-	performAttack(args.mountloc, args.servername, args.username)
+	performAttack(args.mountloc, args.serverIP, args.username)
 
