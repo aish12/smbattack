315ae07fccd501063e3fcdbe52d9b08e8f26a1b0
aish12 <ashashidhar@utexas.edu>
2018-11-13 20:01:20 -0600

    arguments parsed and shell methods created.

---
+++ smbattack.py
@@ -0,0 +1,25 @@
+import argparse as ag
+import os 
+
+def performAttack(location, servername, username, password=None):
+	if (password is None):
+		print ("Null Password, brute forced to: " + bruteForcePassword())
+	else:
+		print ("password is " + password)
+	os.mkdir(location)
+
+def bruteForcePassword():
+	return "bruteForcePassword"
+parser = ag.ArgumentParser()
+parser.add_argument('--mountloc', type=str, help="Enter the location to mount the system")
+parser.add_argument('--servername', type=str, help="Enter the name of the targer windows server")
+parser.add_argument('--username', type=str, help="Enter the username of the target windows server")
+parser.add_argument('--password', type=str, help="Optional. Enter the password of the target windows server. If not included, password will be brute forced.")
+
+
+args = parser.parse_args()
+if args.password:
+	performAttack(args.mountloc, args.servername, args.username, args.password)
+else:
+	performAttack(args.mountloc, args.servername, args.username)
+
